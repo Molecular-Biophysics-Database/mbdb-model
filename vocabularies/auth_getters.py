e4d5755cbafb86fa0c7ebdc34bc6b973ddf8c3db
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2023-07-21 12:19:06 +0200

    bugfix - auth_getter_grant is more robust, and auth_getter_organisms is slightly faster

--- vocabularies/auth_getters.py
+++ vocabularies/auth_getters.py
@@ -2,11 +2,12 @@ import requests
 
 def auth_getter_affiliations(q: str, page: int, size=20, **kwargs):
     # size for this API is fixed to 20
+
     url = 'https://api.ror.org/organizations'
     params = {"query": q,
               "page": page}
-
     response = requests.get(url, params=params)
+
     affiliations = []
     for hit in response.json()['items']:
         affiliations.append(
@@ -22,28 +23,30 @@ def auth_getter_affiliations(q: str, page: int, size=20, **kwargs):
 
     return affiliations
 
-def auth_getter_organisms(q: str, page, size, **kwargs):
+def auth_getter_organisms(q: str, page=1, size=20, **kwargs):
+    # paging and size cannot be supplied and I can't find documentation on how many results
+    # can maximally be returned
 
-    # paging and size cannot be supplied and I can't find good documentation on how many results
-    # maximally can be returned
-
-    # This endpoint is slow and do not appear to return nearly as many hits as expected
+    # This query endpoint returns fewer hits than expected
     search_url = f"https://api.ncbi.nlm.nih.gov/datasets/v1/taxonomy/taxon_suggest/{q}"
+    suggested = requests.get(search_url)
+    taxids = [hit["tax_id"] for hit in suggested.json()["sci_name_and_ids"]]
 
+    # This endpoint is rather slow
     taxid_url = "https://api.ncbi.nlm.nih.gov/datasets/v1/taxonomy/taxon/"
     taxid_params = {"returned_content": "TAXIDS"}
 
-    response = requests.get(search_url)
+    response = requests.get(f"{taxid_url}{','.join(taxids)}", params=taxid_params)
 
     organisms = []
-    for hit in response.json()["sci_name_and_ids"]:
-        taxid = hit["tax_id"]
-        taxid_lookup = requests.get(f"{taxid_url}{taxid}", params=taxid_params)
+    for hit in response.json()["taxonomy_nodes"]:
+        hit = hit["taxonomy"]
         organisms.append(
-            {   "title": {"en": hit["sci_name"]},
+            {
+                "title": {"en": hit["organism_name"]},
                 "props": {
                     "authoritative_id": hit["tax_id"],
-                    "rank": taxid_lookup.json()['taxonomy_nodes'][0]['taxonomy']["rank"]
+                    "rank": hit["rank"]
                 }
             }
         )
@@ -52,7 +55,7 @@ def auth_getter_organisms(q: str, page, size, **kwargs):
 
 
 def auth_getter_grants(q: str, page, size, **kwargs):
-    ### DON'T USE as it occasionally fails
+    # both page and size can be specified for the endpoint
     url = 'https://api.openaire.eu/search/projects'
 
     params = {"keywords": q,
@@ -61,22 +64,26 @@ def auth_getter_grants(q: str, page, size, **kwargs):
               "format": "json"}
 
     response = requests.get(url=url, params=params)
-    #return response
 
     grants = []
     for hit in response.json()["response"]["results"]["result"]:
         project = hit["metadata"]["oaf:entity"]["oaf:project"]
 
+        funding_tree = project["fundingtree"]
+        if not isinstance(funding_tree, list):
+            funding_tree = [funding_tree]
+
+        funders = set([tree["funder"]["name"]["$"] for tree in funding_tree])
+        funder_name = ' and '.join(funders)
+
         grants.append(
             {
                 "title":
-                    {"en": project["title"]},
+                    {"en": project["title"]["$"]},
                 "props": {
-                    "authoritative_id": project["code"],
-                    "funder_name": project["fundingtree"]["funder"]["name"]
-
+                    "authoritative_id": project["code"]["$"],
+                    "funder_name": funder_name
                 }
-
             }
         )
     return grants
