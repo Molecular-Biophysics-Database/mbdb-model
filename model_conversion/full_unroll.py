fbe2a3d20d01444dc1ec51580f4161ff9eb98212
Jitka Plucarova <192219926+jplucarova@users.noreply.github.com>
2026-05-19 15:01:47 +0200

    Add comments + debugging notes

--- model_conversion/full_unroll.py
+++ model_conversion/full_unroll.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+#MODEL_DIR='../models'
+#./full_unroll.py --schema-files $MODEL_DIR/values-only/*.yaml --includes $MODEL_DIR/values-only/general_parameters.yaml
+
 import sys
 from argparse import ArgumentParser
 from copy import deepcopy
@@ -78,6 +82,8 @@ class YamaleTree:
         for key, value, level in self._walk_tree(self.tree):
             indentation = "  |  " * level
             summary = self._value_summary(value)
+            #debugging
+            #print(f"key: {key}, value: {value}, summary: {summary}, is_required: {value.is_required if issubclass(value.__class__, yamale.validators.Validator) else 'N/A'}, choose: {isinstance(value, ExpandedChoose)}, simple_dict: {isinstance(value, dict)}")
             line = f"{indentation} {key}"
             line = f'{line} {(90 - len(line)) * " "} {summary} "\n"'
             tree_lines += line
@@ -87,6 +93,11 @@ class YamaleTree:
 
     @staticmethod
     def _value_summary(value) -> Tuple[str, str, List[str], dict]:
+        # #debugging
+        # val = "value: " + str(value) + "\n"
+        # p = Path('/home/plucarovaj/Documents/for-sync/mbdb-testing/systematic-testing/unroll')
+        # with open(p.joinpath("value.txt"), "w") as f:
+        #     f.write(val)
         """Helper function to extract summary information from yamale objects"""
         if isinstance(value, ExpandedChoose):
             value = value.validator
@@ -96,10 +107,24 @@ class YamaleTree:
         value_types = [type(value).__name__]
         value_constraints = {}
 
+        # #debugging
+        # if not isinstance(value, dict):
+        #     val = "value: " + str(value) + "\n"
+        #     with open(p.joinpath("non-dict.txt"), "a") as f:
+        #         f.write(val)
+
         if isinstance(value, dict):
+            # #debugging
+            # val = "value: " + str(value) + "\n"
+            # with open(p.joinpath("dict.txt"), "a") as f:
+            #     f.write(val)
             value_importance = "required"
 
         if issubclass(value.__class__, yamale.validators.Validator):
+            # #debugging
+            # val = "value: " + str(value) + "\n"
+            # with open(p.joinpath("validator.txt"), "a") as f:
+            #     f.write(val)
             value_importance = {True: "required", False: "optional"}[value.is_required]
             value_constraints = value.kwargs
             if value_types[0] == "List":
@@ -143,8 +168,10 @@ class YamaleTree:
 
     def _get_include(self, value, att="dict"):
         """Helper function to extract the content of a yamale include"""
-        if att == "dict":
-            return deepcopy(self.includes[value.include_name].dict)
+        if att == "dict":                                               # example value: Include(('General_parameters',), {})
+            return deepcopy(self.includes[value.include_name].dict)     #for example above: value.include_name = 'General_parameters', 
+                                                                        #self.includes[value.include_name] is the Schema object created for the 'General_parameters' include,
+                                                                        #and self.includes[value.include_name].dict is the original raw schema dict for that include, which is what we want to insert into the tree to replace the include reference.
         elif att == "_schema":
             return deepcopy(self.includes[value.include_name]._schema)
         else:
@@ -215,7 +242,8 @@ class YamaleTree:
             elif isinstance(value, validators.Include):
                 ## debugging
                 # print(f'direct: {key}')
-                include = self._get_include(value)
+                is_required = value.is_required if hasattr(value, "is_required") else "N/A"
+                include = self._get_include(value) # this is the content of the include, which can be a dict (the original schema) or a yamale validator object depending on the structure of the included schema
                 if isinstance(include, str):
                     include = self._get_include(value, "_schema")
                 if self._is_choose_validator(include):
@@ -296,9 +324,19 @@ def main():
     args = _mk_arg_parser().parse_args()
     for path in args.schema_files:
         yt = YamaleTree(path)
+        #debugging
+        # print(yt.schema.includes) #i1_initial_includes.txt
         if args.includes:
             yt.add_external_includes(*args.includes)
+        #debugging
+        #print(yt.schema.includes) #i2_with_external_includes.txt
+        #for key, value in yt.tree.items():
+        #     print(f"key: {key}, value: {value}, value_name: {value.include_name}, referred_include: {yt.includes[value.include_name].dict}, value_required: {value.is_required if issubclass(value.__class__, yamale.validators.Validator) else 'N/A'}") #yt_before_build.txt
+        #     #self.includes[value.include_name].dict
         yt.build()
+        #debugging
+        # for key, value in yt.tree.items():
+        #     print(f"key: {key}, value: {value}, value_type: {type(value)}, value_required: {value.is_required if issubclass(value.__class__, yamale.validators.Validator) else 'N/A'}") #yt_after_build.txt
         parent, name = new_filename(path)
         if args.output_folder:
             parent = args.output_folder
